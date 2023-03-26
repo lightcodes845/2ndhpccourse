704d6e9a7ebb446154d7480d219c52dbdcf48b41
lightcodes845 <dare.falola7@gmail.com>
2023-03-26 21:51:20 +0100

    first commit

---
+++ kubernetes/backend/src/createdb.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE taskmanagement
+    WITH
+    OWNER = test_user
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
